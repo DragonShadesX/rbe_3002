023f213ce362e392b511895bd1dd4e3177e275f2
dragonshadesx <ggtighe@gmail.com>
2015-04-15 00:25:18 -0400

    made some fixes to account for diagonal moves and stuff

--- point2point_move_function.py
+++ point2point_move_function.py
@@ -1,37 +1,25 @@
 #!/usr/bin/env python
 
+from numpy import *
+from math import *
 
 global driveSpeed 
 driveSpeed = .2
 
-global curT
-
-# move from to 
-# current x, y;  goal x, y;  unit scaling factor
+# move from current position to goal x, y:
 # curT must be with respect to the world with zero along the positive x axis
 # this assumes rotate() is with repect to the robot
 # if world frame replace argument with number in comment following rotate function
-def point2point(curX, curY, goalX, goalY, unit_size):
-    moveX = goalX - curX 
-    moveY = goalY - curY
-    if moveX != 0 and moveY != 0:
-        raise NameError('Diagonal Move Attempted, ignoring...')
-        break
-    if moveX > 0:
-        rotate(-curT)  # 0
-        driveStraight(driveSpeed, moveX*unit_size)
-        curT = 0
-    if moveX < 0:
-        rotate(3.14 - curT)  # 3.14
-        driveStraight(driveSpeed, abs(moveX*unit_size))
-        curT = 3.14
-    if moveY > 0:
-        rotate(-1.57 - curT)  # -1.57
-        driveStraight(driveSpeed, moveY*unit_size)
-        curT = -1.57
-    if moveY < 0:
-        rotate(1.57 - curT)  # 1.57
-        driveStraight(driveSpeed, abs(moveY*unit_size))
-        curT = 1.57
+def point2point(goalX, goalY):
+    moveX = goalX - trans.x
+    moveY = goalY - trans.y
 
+    #This block to get rid of divide by zero errors
+    if moveX == 0:
+        moveX +=.01
+    if moveY == 0:
+        moveY +=.01
 
+    rotate(-atan(moveY / moveX))
+    driveStraight(driveSpeed, (moveX**2 + moveY**2)**.5)
+    return
